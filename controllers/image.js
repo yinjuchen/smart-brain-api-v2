bf2f99fb18f7e9d91acda6fe73909071d09afabe
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro.local>
2022-10-08 08:07:20 +0800

    adding server for smartbrain project

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
+  apiKey: 'da8b8f7d4c7d4b2bbaa7ae05b4d4099f'
 });
 
 
@@ -27,16 +27,16 @@ const handleApiCall = (req, res) => {
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+      // entries[0] --> this used to return the entries
+      // TO
+      // entries[0].entries --> this now returns the entries
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
